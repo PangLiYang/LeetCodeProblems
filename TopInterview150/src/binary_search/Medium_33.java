248d18914023c3c236faf64bcf7f83e429df7d40
pangli <py2236@nyu.edu>
2024-05-30 19:56:40 -0400

    0530

--- TopInterview150/src/binary_search/Medium_33.java
+++ TopInterview150/src/binary_search/Medium_33.java
@@ -1,6 +1,6 @@
-import java.util.*;
+package binary_search;
 
-public class Test {
+public class Medium_33 {
 
     public int search(int[] nums, int target) {
         return subV(nums, target, 0, nums.length - 1);
@@ -49,19 +49,5 @@ public class Test {
         }
 
         return -1;
-    }
-
-    public static void main(String[] args) {
-
-        Test tt = new Test();
-
-        int[] nums = {4,5,6,7,0,1,2};
-
-        int tar = 3;
-
-        int o = tt.search(nums, tar);
-
-        System.out.println(o);
-
     }
 }
